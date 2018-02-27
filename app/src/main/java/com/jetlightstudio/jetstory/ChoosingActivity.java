5680a1c757df99b73ec0f745f2281cde891864b0
Oussama BONNOR <oussamabonnor@gmail.com>
2018-02-27 22:15:53 +0100

    adding first spinner drop down

--- app/src/main/java/com/jetlightstudio/jetstory/ChoosingActivity.java
+++ app/src/main/java/com/jetlightstudio/jetstory/ChoosingActivity.java
@@ -2,12 +2,21 @@ package com.jetlightstudio.jetstory;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class ChoosingActivity extends AppCompatActivity {
+
+    Spinner spinnerCategory;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choosing);
+        spinnerCategory = (Spinner) findViewById(R.id.spinnerCategory);
+        ArrayAdapter<CharSequence> arr = ArrayAdapter.createFromResource(this, R.array.category, android.R.layout.simple_spinner_item);
+        arr.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerCategory.setAdapter(arr);
+
     }
 }
