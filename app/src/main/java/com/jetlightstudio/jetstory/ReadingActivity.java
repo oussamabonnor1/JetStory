fc359b39e5b707edefc2026fb46325eab88d1f03
Oussama BONNOR <oussamabonnor@gmail.com>
2018-02-27 19:12:25 +0100

    fixing errors in java code

--- app/src/main/java/com/jetlightstudio/jetstory/ReadingActivity.java
+++ app/src/main/java/com/jetlightstudio/jetstory/ReadingActivity.java
@@ -90,10 +90,6 @@ public class ReadingActivity extends AppCompatActivity
 
         } else if (id == R.id.nav_manage) {
 
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
