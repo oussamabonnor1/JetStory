f8ffaa91855608415021ab8a43aca50330963d00
Oussama BONNOR <oussamabonnor@gmail.com>
2018-02-27 22:42:06 +0100

    second activity design updated

--- app/src/main/java/com/jetlightstudio/jetstory/ChoosingActivity.java
+++ app/src/main/java/com/jetlightstudio/jetstory/ChoosingActivity.java
@@ -8,6 +8,7 @@ import android.widget.Spinner;
 public class ChoosingActivity extends AppCompatActivity {
 
     Spinner spinnerCategory;
+    Spinner spinnerLength;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +18,11 @@ public class ChoosingActivity extends AppCompatActivity {
         ArrayAdapter<CharSequence> arr = ArrayAdapter.createFromResource(this, R.array.category, android.R.layout.simple_spinner_item);
         arr.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerCategory.setAdapter(arr);
+
+        spinnerLength = (Spinner) findViewById(R.id.spinnerLength);
+        ArrayAdapter<CharSequence> arr2 = ArrayAdapter.createFromResource(this, R.array.length, android.R.layout.simple_spinner_item);
+        arr2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerLength.setAdapter(arr2);
 
     }
 }
