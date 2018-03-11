24e646353357d56b11ca79ed5cb7d91285b9e4e1
Oussama BONNOR <oussamabonnor@gmail.com>
2018-03-11 21:42:28 +0100

    trying out different APIs

--- app/src/main/java/com/jetlightstudio/jetstory/ChoosingActivity.java
+++ app/src/main/java/com/jetlightstudio/jetstory/ChoosingActivity.java
@@ -4,11 +4,12 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
-import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -25,7 +26,7 @@ public class ChoosingActivity extends AppCompatActivity {
     Spinner spinnerCategory;
     Spinner spinnerLength;
     ArrayList<Story> stories;
-    String content;
+    JSONObject json;
 
 
     @Override
@@ -43,32 +44,8 @@ public class ChoosingActivity extends AppCompatActivity {
         spinnerLength.setAdapter(arr2);
         stories = new ArrayList<>();
 
-
         final RetreiveStoryData storyData = new RetreiveStoryData();
         storyData.execute();
-
-        for (int i = 0; i < 25; i++) {
-            int c = new Random().nextInt(5);
-            Story.Category category = Story.Category.action;
-            switch (c) {
-                case 0:
-                    category = Story.Category.action;
-                    break;
-                case 1:
-                    category = Story.Category.comedy;
-                    break;
-                case 2:
-                    category = Story.Category.romance;
-                    break;
-                case 3:
-                    category = Story.Category.moral;
-                    break;
-                case 4:
-                    category = Story.Category.sad;
-                    break;
-            }
-            stories.add(new Story("story " + (i + 1), "author " + (i + 1), "DD/MM/YYYY", i + 1, new Random().nextInt(10) + 1, category));
-        }
     }
 
     public void searchStory(View view) {
@@ -122,16 +99,15 @@ public class ChoosingActivity extends AppCompatActivity {
         }
         Intent i = new Intent(this, StoryListActivity.class);
         i.putExtra("stories", storiesTemp);
-        i.putExtra("content", content);
         startActivity(i);
     }
 
     public class RetreiveStoryData extends AsyncTask<Void, Void, Void> {
         String data;
+        int index = 0;
 
         @Override
         protected Void doInBackground(Void... voids) {
-            System.out.println("fetching...");
             try {
                 URL url = new URL("https://api.chucknorris.io/jokes/random");
                 HttpURLConnection connection = (HttpURLConnection) url.openConnection();
@@ -139,7 +115,6 @@ public class ChoosingActivity extends AppCompatActivity {
                 BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
                 String s = "";
                 while (s != null) {
-                    System.out.println("got: " + s);
                     s = bufferedReader.readLine();
                     data += s;
                 }
@@ -154,33 +129,44 @@ public class ChoosingActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(Void aVoid) {
             super.onPostExecute(aVoid);
-            content = data;
-        }
-
-        /*// Do some validation here
             try {
-                URL url = new URL("https://api.androidhive.info/contacts/");
-                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-                urlConnection.setRequestMethod("GET");
-                urlConnection.connect();
-
-                try {
-                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
-                    StringBuilder stringBuilder = new StringBuilder();
-                    String line;
-                    while ((line = bufferedReader.readLine()) != null) {
-                        stringBuilder.append(line).append("\n");
-                    }
-                    bufferedReader.close();
-                    s = stringBuilder.toString();
-                } finally {
-                    urlConnection.disconnect();
-                }
-            } catch (Exception e) {
-                Log.e("ERROR", e.getMessage(), e);
-                return null;
+                data = data.replace("null","");
+                json = new JSONObject(data);
+            } catch (JSONException e) {
+                e.printStackTrace();
             }
-            return null;*/
+
+
+            int c = new Random().nextInt(5);
+            Story.Category category = Story.Category.action;
+            switch (c) {
+                case 0:
+                    category = Story.Category.action;
+                    break;
+                case 1:
+                    category = Story.Category.comedy;
+                    break;
+                case 2:
+                    category = Story.Category.romance;
+                    break;
+                case 3:
+                    category = Story.Category.moral;
+                    break;
+                case 4:
+                    category = Story.Category.sad;
+                    break;
+            }
+            stories.add(new Story("story " + (stories.size()), "author " + stories.size(), "DD/MM/YYYY", stories.size(), new Random().nextInt(10) + 1, category));
+
+            try {
+                stories.get(stories.size() - 1).setContent(json.get("value").toString());
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            System.out.println(stories.get(stories.size()-1));
+            index++;
+            if(index < 25) this.execute();
+        }
     }
 }
 
