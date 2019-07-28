93aaddbe2e853dbaf4542d92a61c8b7c8e4186a0
oussamabonnor1 <oussamabonnor@gmail.com>
2019-07-28 02:18:36 +0100

    adding image view in reading activity (story cover)

--- app/src/main/java/com/jetlightstudio/jetstory/ToolBox/StoryApiManager.java
+++ app/src/main/java/com/jetlightstudio/jetstory/ToolBox/StoryApiManager.java
@@ -20,7 +20,7 @@ public class StoryApiManager extends AsyncTask<Void, Void, ArrayList<Story>> {
 
     ArrayList<Story> stories = new ArrayList<>();
     String data;
-    String path = "http://617347c5.ngrok.io";
+    String path = "http://3a8a462e.ngrok.io";
 
     @Override
     protected ArrayList<Story> doInBackground(Void... voids) {
