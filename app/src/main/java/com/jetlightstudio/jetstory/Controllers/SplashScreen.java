feea09821b8211fe995cd863d86be069ff66fa5a
oussamabonnor1 <oussamabonnor@gmail.com>
2019-08-03 22:06:44 +0100

    adding splash screen activity

---
+++ app/src/main/java/com/jetlightstudio/jetstory/Controllers/SplashScreen.java
@@ -0,0 +1,15 @@
+package com.jetlightstudio.jetstory.Controllers;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.jetlightstudio.jetstory.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
